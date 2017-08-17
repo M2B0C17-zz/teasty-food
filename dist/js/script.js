dac6529d6d6a9bb3ec31645043b1b9612f170ff5
Syrimande <caterinadasilva@gmail.com>
2017-08-17 11:18:47 -0300

    profile listo para recibir datos desde Localstorage

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,43 +20253,37 @@ if (jQuery) {
   };
 })(jQuery);
 
-  function handleFileSelect(evt) {
+function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
     // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            continue;
+        }
 
-      // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
+        var reader = new FileReader();
 
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
         return function(e) {
-          // Render thumbnail.
-          var span = document.getElementById('list');
-          span.innerHTML = '<img class="prof-pic" src="' + e.target.result +
-                            '" title="' + escape(theFile.name) + '"/>';
-            
-          localStorage.setItem('img', e.target.result);
-        };
-      })(f);
+            // Render thumbnail.
+            var span = document.getElementById('list');
+            span.innerHTML = '<img class="prof-pic" src="' + e.target.result + '" title="' + escape(theFile.name) + '"/>';
 
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+            localStorage.setItem('img', e.target.result);
+            };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
     }
-  }
+}
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-
-  if(localStorage.img) { 
-
-         var span = document.getElementById('list');
-          span.innerHTML = '<img class="prof-pic" src="'+ localStorage.img +
-                            '" title="test"/>';
-    
-    }
+if(localStorage.img) { 
+    var span = document.getElementById('list');
+    span.innerHTML = '<img class="prof-pic" src="'+ localStorage.img +'" title="test"/>';
+}
